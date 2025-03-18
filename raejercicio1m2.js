19676848a57c9d069af43b7531638e03a1c7386f
AnaRaquelcv <anaraquel2004cv@gmail.com>
2025-03-18 10:26:46 -0400

    Add files via upload

---
+++ raejercicio1m2.js
@@ -0,0 +1,9 @@
+let edad = 39;
+if (edad > 40) {
+    console.log("es mayor de 40 años de edad");}
+    else if (edad <40 && edad >=1){ 
+        console.log("es menor de 40 años de edad");}
+    else if (edad <1){ 
+        console.log("ingrese un numero mayor a 0");}
+    else{ 
+        console.log("adulto mayor")}
---
+++ raejercicio2m2.js
@@ -0,0 +1,6 @@
+let texto ="daniel"; //string
+let persona ={materia: "programacion modulo", nivel:2}; //object
+let numero ="25"; //number
+const texto1 ="maracaibo"; //string
+
+console.log("el estudiante del curso de " +persona, "se llama "+texto, "su edad es de "+numero, "años y vive en la ciudad de "+texto1,);
