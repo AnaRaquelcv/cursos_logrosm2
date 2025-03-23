07e010f8f68e845627d7679db1549cf3f364a5d7
AnaRaquelcv <anaraquel2004cv@gmail.com>
2025-03-23 17:18:59 -0400

    Add files via upload

---
+++ indexraejercicio13m2.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Calculadora simple</title>
+</head>
+<body>
+
+<form method="post" action="">
+    ingrese una operacion matematica como suma +, resta -, multiplicacion *,  o division/ : <input type="text" name="operacion" required><br>
+    ingrese el primer numero: <input type="number" name="num1" required><br>
+    ingrese el segundo numero: <input type="number" name="num2" required><br>
+
+    <input type="submit" value="mostrar">
+</for<m>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recogemos los valores del formulario
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $operacion = $_POST['operacion'];
+
+        // Calculamos el mayor
+        $suma = $num1 + $num2;
+        $resta = $num1 - $num2;
+        $multiplicacion = $num1 * $num2;
+        $division = $num1 / $num2;
+    
+
+    switch ($operacion) {
+        case "suma":
+            echo "<p>El resultado de la suma es: $suma</p>";
+            break;
+        case "resta";
+            echo "<p>El resultado de la resta es: $$resta</p>";
+            break;
+        case "multiplicacion";
+            echo "<p>El resultado de la multiplicacion es: $$multiplicacion</p>";
+            break;
+        case "division";
+            echo "<p>El resultado de la division es: $$division</p>";
+            break;
+        default:
+            echo "<p>La opcion ingresada no es valida</p>";
+            break;
+    }
+
+
+}
+?>
+
+</body>
+</html>
+
---
+++ indexraejercicio14m2.php
@@ -0,0 +1,15 @@
+
+<?php
+
+    $contador = 2;
+    for ($i = 1; $i<=100; $i++){
+    if ($i%2==0){
+        echo "<p>$i</p>";
+    
+
+    }
+}
+?>
+
+</body>
+</html>
